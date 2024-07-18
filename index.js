6aeb56bca663882a8052bff3631d86b6fe7b1ea2
olwpad <olwpad@gmail.com>
2024-07-17 20:04:49 -0500

    project uploaded

---
+++ index.js
@@ -0,0 +1,47 @@
+const Encriptar = () => {
+  const elementos = document.querySelector(".entrada");
+
+  if (elementos.value == "") return;
+  let concatenar = "";
+
+  for (const valor of elementos.value) {
+    if (valor === "e") {
+      concatenar += "enter";
+    } else if (valor === "i") {
+      concatenar += "imes";
+    } else if (valor === "a") {
+      concatenar += "ai";
+    } else if (valor === "o") {
+      concatenar += "ober";
+    } else if (valor === "u") {
+      concatenar += "ufat";
+    } else {
+      concatenar += valor;
+    }
+  }
+  const valor1 = document.querySelector(".textoChange");
+  const valor2 = document.querySelector(".textoChange1");
+  valor1.textContent = concatenar;
+  valor2.textContent = "Has encriptado el texto";
+  borradorDeTexto(elementos);
+};
+
+const borradorDeTexto = (elementos) => {
+  elementos.value = "";
+};
+
+const Desencriptar = () => {
+  const elementos = document.querySelector(".textoChange");
+  let desencriptar = elementos.innerHTML;
+  desencriptar = desencriptar
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+  const valor1 = document.querySelector(".textoChange");
+  const valor2 = document.querySelector(".textoChange1");
+  valor1.textContent = desencriptar;
+  if (valor2.innerHTML == "Ingresa el texto que desees desencriptar") return;
+  valor2.textContent = "Has desencriptado el texto";
+};
